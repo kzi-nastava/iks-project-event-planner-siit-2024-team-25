dff689eacdc02bc81bbb935f9e10b6b29c8256b4
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-11-19 11:22:37 +0100

    [ADD] Card component for single event [REFACTOR] Top 5 event display

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeTopEventsComponent } from './home-top-events/home-top-events.component';
 import { MaterialModule } from '../infrastructure/material/material.module';
-
+import { HomeAllEventsComponent } from './home-all-events/home-all-events.component';
+import { HomeEventCardComponent } from './home-event-card/home-event-card.component';
+import { OfferingModule } from "../offering/offering.module";
 
 
 @NgModule({
   declarations: [
-    HomeTopEventsComponent
+    HomeTopEventsComponent,
+    HomeAllEventsComponent,
+    HomeEventCardComponent
   ],
   imports: [
     CommonModule,
-    MaterialModule
-  ],
+    MaterialModule,
+    OfferingModule
+],
   exports: [
     HomeTopEventsComponent
   ]
