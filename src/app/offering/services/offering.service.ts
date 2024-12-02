7f0bf71f4336ef0063bea87a0a08dca4caa40578
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-12-02 19:52:00 +0100

    [ADD] All offerings display

--- src/app/offering/services/offering.service.ts
+++ src/app/offering/services/offering.service.ts
@@ -1,130 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HomeOffering } from '../model/home-offering.model';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 import { OfferingFilterParams } from '../model/home-offering-filter-params-model';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Page } from '../../shared/model/page.mode';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class OfferingService {
-  
-
-  constructor() { }
-
-  private currentOfferings: HomeOffering[] = [
-    {
-      id: 1,
-      name: 'Event Photography',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 1500,
-      description: 'Professional photography services for events, capturing memorable moments.',
-      rating: 5.0,
-      country: "Republika Srbija"
-    },
-    {
-      id: 2,
-      name: 'Party Decorations',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 800,
-      description: 'Custom event decorations to match the theme of your celebration.',
-      rating: 5.0,
-      country: "Republika Srbija"
-    },
-    {
-      id: 3,
-      name: 'Live Band Performance',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 3000,
-      description: 'Live band performances for weddings, corporate events, and parties.',
-      rating: 5.0,
-      country: "Republika Srbija"
-    },
-    {
-      id: 4,
-      name: 'Wedding Catering',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 2500,
-      description: 'Complete catering service for weddings, including a variety of dishes and drinks.',
-      rating: 5.0,
-      country: "Republika Srbija"
-
-    },
-    {
-      id: 5,
-      name: 'Transportation Services',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 1000,
-      description: 'Transportation services for guests, including shuttle buses and luxury cars.',
-      rating: 5.0,
-      country: "Republika Srbija"
-    },
-    {
-      id: 6,
-      name: 'Event Photography',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 1500,
-      description: 'Professional photography services for events, capturing memorable moments.',
-      rating: 5.0,
-      country: "Republika Srbija"
-    },
-    {
-      id: 7,
-      name: 'Party Decorations',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 800,
-      description: 'Custom event decorations to match the theme of your celebration.',
-      rating: 5.0,
-      country: "Republika Srbija"
-    },
-    {
-      id: 8,
-      name: 'Live Band Performance',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 3000,
-      description: 'Live band performances for weddings, corporate events, and parties.',
-      rating: 5.0,
-      country: "Republika Srbija"
-    },
-    {
-      id: 9,
-      name: 'Wedding Catering',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 2500,
-      description: 'Complete catering service for weddings, including a variety of dishes and drinks.',
-      rating: 5.0,
-      country: "Republika Srbija"
-
-    },
-    {
-      id: 10,
-      name: 'Transportation Services',
-      city: 'Uzice',
-      ownerName: 'Stefan',
-      isFavourite: false,
-      price: 1000,
-      description: 'Transportation services for guests, including shuttle buses and luxury cars.',
-      rating: 5.0,
-      country: "Republika Srbija"
-    }
-  ];
+  constructor(private httpClient: HttpClient) {}
 
   private currentTopOfferings: HomeOffering[] = [
     {
@@ -134,9 +19,10 @@ export class OfferingService {
       ownerName: 'Stefan',
       isFavourite: false,
       price: 1500,
-      description: 'Professional photography services for events, capturing memorable moments.',
+      description:
+        'Professional photography services for events, capturing memorable moments.',
       rating: 5.0,
-      country: "Republika Srbija"
+      country: 'Republika Srbija',
     },
     {
       id: 12,
@@ -145,9 +31,10 @@ export class OfferingService {
       ownerName: 'Stefan',
       isFavourite: false,
       price: 800,
-      description: 'Custom event decorations to match the theme of your celebration.',
+      description:
+        'Custom event decorations to match the theme of your celebration.',
       rating: 5.0,
-      country: "Republika Srbija"
+      country: 'Republika Srbija',
     },
     {
       id: 13,
@@ -156,9 +43,10 @@ export class OfferingService {
       ownerName: 'Stefan',
       isFavourite: false,
       price: 3000,
-      description: 'Live band performances for weddings, corporate events, and parties.',
+      description:
+        'Live band performances for weddings, corporate events, and parties.',
       rating: 5.0,
-      country: "Republika Srbija"
+      country: 'Republika Srbija',
     },
     {
       id: 14,
@@ -167,10 +55,10 @@ export class OfferingService {
       ownerName: 'Stefan',
       isFavourite: false,
       price: 2500,
-      description: 'Complete catering service for weddings, including a variety of dishes and drinks.',
+      description:
+        'Complete catering service for weddings, including a variety of dishes and drinks.',
       rating: 5.0,
-      country: "Republika Srbija"
-
+      country: 'Republika Srbija',
     },
     {
       id: 15,
@@ -179,29 +67,33 @@ export class OfferingService {
       ownerName: 'Stefan',
       isFavourite: false,
       price: 1000,
-      description: 'Transportation services for guests, including shuttle buses and luxury cars.',
+      description:
+        'Transportation services for guests, including shuttle buses and luxury cars.',
       rating: 5.0,
-      country: "Republika Srbija"
-    }
+      country: 'Republika Srbija',
+    },
   ];
 
   getTopOfferings(): Observable<HomeOffering[]> {
     return of(this.currentTopOfferings);
   }
 
-  getOfferings(page: number){
-    
-    return of(this.currentOfferings);
-  }
+  getOfferings(
+    page: number
+  ): Observable<{ currentOfferings: HomeOffering[]; totalPages: number }> {
+    let params = new HttpParams();
 
-  getFilteredOfferings(filterParams: OfferingFilterParams): Observable<HomeOffering[]> {
-    
-    console.log(JSON.stringify(filterParams))
-    return of(this.currentOfferings.filter(o => {
-      if(filterParams.name){
-        return o.name.includes(filterParams.name);
-      }
-      return true;
-    }));
+    params = params.set('page', page);
+
+    return this.httpClient
+      .get<Page<HomeOffering>>('http://localhost:8080/api/offerings/', {
+        params,
+      })
+      .pipe(
+        map((page) => ({
+          currentOfferings: page.content,
+          totalPages: page.totalPages,
+        }))
+      );
   }
 }
