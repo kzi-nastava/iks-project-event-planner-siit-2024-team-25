0a48c5c94cf79f75717da47653a05269487b51de
Stefan Stjepic <stefan.stjepic03@gmail.com>
2024-12-06 22:05:39 +0100

    [ADD] Refresh button

--- src/app/event/home-event-filter/home-event-filter.component.ts
+++ src/app/event/home-event-filter/home-event-filter.component.ts
@@ -67,4 +67,9 @@ export class HomeEventFilterComponent implements OnInit {
   searchEvents(): void {
     this.clicked.emit(this.filterParams);
   }
+
+  refreshEventFilter() {
+    this.filterParams = {};
+    this.clicked.emit(this.filterParams);
+  }
 }
