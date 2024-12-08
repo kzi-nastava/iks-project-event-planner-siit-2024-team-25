8d49652fcb7ee4378b3106a606dfa889324122f4
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-12-08 16:38:00 +0100

    [ADD] Route guards to routes

--- src/app/offering/service/service-routing.module.ts
+++ src/app/offering/service/service-routing.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { roleGuard } from '../../infrastructure/auth/guard/role.guard';
+import { UserRole } from '../../infrastructure/auth/model/user-role.model';
 import { ListServicesComponent } from './list-services/list-services.component';
 import { ServiceDetailsComponent } from './service-details/service-details.component';
 import { ServiceFormComponent } from './service-form/service-form.component';
-import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'services', component: ListServicesComponent },
-  {path : 'services/:id', component:ServiceDetailsComponent},
-  {path:'serviceForm/:id' , component:ServiceFormComponent},
-  {path :'serviceForm', component: ServiceFormComponent},
-]
+  { path: 'services/:id', component: ServiceDetailsComponent },
+  {
+    path: 'serviceForm/:id',
+    component: ServiceFormComponent,
+    canActivate: [roleGuard],
+    data: { roles: [UserRole.Owner] },
+  },
+  {
+    path: 'serviceForm',
+    component: ServiceFormComponent,
+    canActivate: [roleGuard],
+    data: { roles: [UserRole.Owner] },
+  },
+];
 
 @NgModule({
-  imports: [ RouterModule.forChild(routes)],
-  exports:[RouterModule]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class ServiceRoutingModule { }
+export class ServiceRoutingModule {}
--- src/app/user/user-routing.module.ts
+++ src/app/user/user-routing.module.ts
@@ -1,15 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { unauthenticatedGuard } from '../infrastructure/auth/guard/unauthenticated.guard';
 import { ActivateComponent } from './activate/activate.component';
 
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
-  { path: 'activate', component: ActivateComponent },
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [unauthenticatedGuard],
+  },
+  {
+    path: 'register',
+    component: RegisterComponent,
+    canActivate: [unauthenticatedGuard],
+  },
+  {
+    path: 'activate',
+    component: ActivateComponent,
+  },
 ];
 
 @NgModule({
