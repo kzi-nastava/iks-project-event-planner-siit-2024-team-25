5b8baf215ec6379b67829a47c1bb136c9bebe690
Nikola Jolovic <nikolajolovic02@gmail.com>
2024-12-28 11:37:13 +0100

    [ADD] Map component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { CommonModule, NgOptimizedImage } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { NgxDropzoneModule } from 'ngx-dropzone';
 import { MaterialModule } from '../infrastructure/material/material.module';
-import { ImageUploadComponent } from './image-upload/image-upload.component';
 import { ErrorDialogComponent } from './error-dialog/error-dialog.component';
+import { ImageUploadComponent } from './image-upload/image-upload.component';
+import { MapComponent } from './map/map.component';
 
-const components = [ImageUploadComponent];
+const components = [ImageUploadComponent, MapComponent];
 
 @NgModule({
   declarations: [...components, ErrorDialogComponent],
