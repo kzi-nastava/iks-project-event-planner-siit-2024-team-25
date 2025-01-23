3ddab50e0eab7f9590745328036df62ba30c1443
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-01-23 23:07:30 +0100

    [REFACTOR] Offering card display [ADD] More details button

--- src/app/offering/home-offering-card/home-offering-card.component.ts
+++ src/app/offering/home-offering-card/home-offering-card.component.ts
@@ -1,17 +1,16 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { HomeOffering } from '../model/home-offering.model';
 import { DecimalPipe } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-offering-card',
   templateUrl: './home-offering-card.component.html',
-  styleUrl: './home-offering-card.component.scss'
+  styleUrl: './home-offering-card.component.scss',
 })
 export class HomeOfferingCardComponent {
-
   @Input()
   offering!: HomeOffering;
-
 
   @Output()
   clicked: EventEmitter<HomeOffering> = new EventEmitter<HomeOffering>();
@@ -20,14 +19,22 @@ export class HomeOfferingCardComponent {
     this.clicked.emit(this.offering);
   }
 
-  constructor(private decimalPipe: DecimalPipe){}
+  constructor(private decimalPipe: DecimalPipe, private router: Router) {}
 
   formatRating(rating: number): string {
-    return this.decimalPipe.transform(rating, '1.1') || ''; 
+    return this.decimalPipe.transform(rating, '1.1') || '';
   }
 
-  formatLocation(country: string, city: string){
-    return country + "," + city;
+  formatLocation(country: string, city: string) {
+    return country + ',' + city;
   }
 
+  showMoreDetails() {
+    console.log(this.offering);
+    if (this.offering.isService) {
+      this.router.navigateByUrl('/service/services/' + this.offering.id);
+    } else {
+      this.router.navigateByUrl('/products/' + this.offering.id);
+    }
+  }
 }
--- src/app/offering/model/home-offering.model.ts
+++ src/app/offering/model/home-offering.model.ts
@@ -1,11 +1,12 @@
-export interface HomeOffering{
-    id: number,
-    name: string,
-    price: number,
-    city: string,
-    country: string,
-    ownerName: string,
-    isFavourite: boolean,
-    description: string,
-    rating: number,
+export interface HomeOffering {
+  id: number;
+  name: string;
+  price: number;
+  city: string;
+  country: string;
+  ownerName: string;
+  isFavourite: boolean;
+  description: string;
+  rating: number;
+  isService: boolean;
 }
